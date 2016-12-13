8820af3b7a51335e3c1b5e424b5787491dd387b0
den9123 <den33rus28@bk.ru>
2016-12-13 21:27:15 +0400

    My first commit

---
+++ UnitTestProject1/UnitTestProject1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    class Class1
+    {
+        public static int Test1(int x1, int x2)
+        {
+            return x1 * x2;
+        }
+
+
+        public static string Test2(string x1, string x2)
+        {
+                  return x1 + x2 ;
+
+        }
+
+        public static int Test3(int x1, int x2)
+        {
+            return x1 - x2;
+        }
+
+        public static int Test4(int x1, int x2)
+        {
+            return x1 / x2;
+        }
+      
+    }
+
+}
---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int q1 = 6;
+           int q2 = Class1.Test1(2, 3);
+            Assert.AreEqual(q1, q2);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string q1 = "Проверка теста";
+            string q2 = Class1.Test2("Проверка ", "теста");
+            Assert.AreEqual(q1, q2);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int q1 = 5;
+            int q2 = Class1.Test3(7, 2);
+            Assert.AreEqual(q1, q2);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int q1 = 4;
+            int q2 = Class1.Test4(12,3);
+            Assert.AreEqual(q1, q2);
+        }
+
+    }
+}
